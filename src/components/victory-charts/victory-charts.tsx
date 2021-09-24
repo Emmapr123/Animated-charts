d45be7cff9e3498b23971039b6094e9c70e61ecb
Emmapr123 <emmapriester@hotmail.com>
2021-09-24 10:11:03 +0100

    First lines for Victory charts

---
+++ src/components/victory-charts/victory-charts.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as V from 'victory';
+
+const VictoryCharts = () => {
+    const data = [
+        {quarter: 1, earnings: 13000},
+        {quarter: 2, earnings: 16500},
+        {quarter: 3, earnings: 14250},
+        {quarter: 4, earnings: 19000}
+      ];
+
+      return(
+          <div>
+
+          </div>
+      )
+
+}
+
+export { VictoryCharts }
